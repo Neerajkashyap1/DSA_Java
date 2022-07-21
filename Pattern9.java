57750dc488c513a681ab075f748dd7b07f6e7153
Neeraj Kashyap <93763513+Neerajkashyap1@users.noreply.github.com>
2022-07-21 09:13:18 +0530

    Create Pattern9.java

---
+++ Pattern9.java
@@ -0,0 +1,16 @@
+public class Pattern9 {
+    public static void main(String[] args) {
+        int num = 0;
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                num = i + j;
+                if (num % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
