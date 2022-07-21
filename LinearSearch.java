4ba9706c9fb018be1fe5d8b7ad5530ef9ba02f33
Neeraj Kashyap <93763513+Neerajkashyap1@users.noreply.github.com>
2022-07-21 09:04:32 +0530

    Linear Searching

---
+++ LinearSearch.java
@@ -0,0 +1,13 @@
+class linearSearch1 {
+    public static void main(String[] args) {
+        int arr[] = { 18, 12, 9, 14, 77, 50 };
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 17)
+                System.out.println("yes 17 exits");
+            else
+                ;
+            System.out.println("no");
+        }
+    }
+}
