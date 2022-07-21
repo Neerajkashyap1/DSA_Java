22861a49707ddc81632ff03bc61e5d78a26435d0
Neeraj Kashyap <93763513+Neerajkashyap1@users.noreply.github.com>
2022-07-21 09:14:38 +0530

    Create Pattern12.java

---
+++ Pattern12.java
@@ -0,0 +1,19 @@
+public class Pattern12 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(i - k + 1);
+            }
+            for (int l = 1; l < i; l++) {
+                System.out.print(l + 1);
+            }
+            System.out.println();
+
+        }
+
+    }
+}
